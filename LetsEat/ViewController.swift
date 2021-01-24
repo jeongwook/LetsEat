4b3fe30b9e211331126c42741a3ca14bfca8ac84
Juan Yu <jeongwook.yu@gmail.com>
2021-01-24 05:35:41 -0800

    Initial Commit

---
+++ LetsEat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsEat
+//
+//  Created by Juan Yu on 1/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
